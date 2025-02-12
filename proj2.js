02a772d033385a8f29bb83f4cafc98b440a7b0a8
Emmaginations <158538580+Emmaginations@users.noreply.github.com>
2025-02-12 16:35:05 -0500

    Assignment3.03

--- proj2.js
+++ proj2.js
@@ -1,8 +1,8 @@
 document.getElementById("submit").addEventListener("click", function(event) {
     event.preventDefault();
 
-    let age = document.getElementById("age");
-    document.getElementById("result").innerHTML = `<p>The age difference between you and Emma is: ${calculateAgeDifference(age)} years.</p>`;
+    let ageElem = document.getElementById("age");
+    document.getElementById("result").innerHTML = `<p>The age difference between you and Emma is: ${calculateAgeDifference(ageElem)} years.</p>`;
 });
 
 
