ead2b5703613ae09a25f96e3ef469f3ce078734d
Emmaginations <158538580+Emmaginations@users.noreply.github.com>
2025-02-12 16:39:01 -0500

    Assignment3.05

--- proj2.js
+++ proj2.js
@@ -2,17 +2,18 @@ document.getElementById("submit").addEventListener("click", function(event) {
     event.preventDefault();
 
     let ageElem = document.getElementById("age");
-    document.getElementById("result").innerHTML = `<p>The age difference between you and Emma is: ${calculateAgeDifference(ageElem)} years.</p>`;
+    let ageVal = parseInt(ageElem.value);
+    document.getElementById("result").innerHTML = `<p>The age difference between you and Emma is: ${calculateAgeDifference(ageVal)} years.</p>`;
 });
 
 
 function calculateAgeDifference(age) {
     if (age > 21) {
         console.log("Computation complete.");
-        return (age.value - 21);
+        return (age - 21);
     } else if (age < 21) {
         console.log("Computation complete.");
-        return (21 - age.value);
+        return (21 - age);
     }
 }
 
