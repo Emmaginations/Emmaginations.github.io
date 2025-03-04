dc919add4f8c23fb7033a6631004dccebc72266e
Emmaginations <158538580+Emmaginations@users.noreply.github.com>
2025-03-03 23:24:15 -0500

    Update proj5.js

--- proj5.js
+++ proj5.js
@@ -2,15 +2,17 @@ let art = document.createElement("img");
 art.src = "../img3.jpg";
 art.alt = "A digital drawing of a woman";
 art.onclick = createOverlay;
-
+// Use of createElement()
 let header = document.createElement("h2");
 let description = document.createTextNode("This is an example of my digital drawing skills.");
 let main_c = document.getElementById("main-content");
 
+// Use of appendChild()
 main_c.appendChild(header);
 header.appendChild(description);
 main_c.appendChild(art);
 
+// Function to create an overlay that replaces the current text image
 function createOverlay() {
     let overlayImage = document.createElement("img");
     overlayImage.src = "../img5.jpg";
@@ -19,7 +21,7 @@ function createOverlay() {
     main_c.appendChild(overlayImage);
 
     overlayImage.onclick = function() {
-        document.body.removeChild(overlayImage);
+        main_c.removeChild(overlayImage);
 
         let art = document.createElement("img");
         art.src = "../img3.jpg";
