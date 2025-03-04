0bd50f1313a994e88b96fe97e33656a8058f008c
Emmaginations <158538580+Emmaginations@users.noreply.github.com>
2025-03-03 22:36:32 -0500

    5.01

---
+++ proj5.js
@@ -0,0 +1,36 @@
+let art = document.createElement("img");
+art.src = "img3.jpg";
+art.alt = "A digital drawing of a woman";
+art.onclick = createOverlay;
+
+let header = document.createElement("h2");
+let description = document.createTextNode("This is an example of my digital drawing skills.");
+let main_c = document.getElementById("main-content");
+
+main_c.appendChild(art);
+main_c.appendChild(header);
+header.appendChild(description);
+
+function createOverlay() {
+	let overlay = document.createElement("div");
+	overlay.id = "artOverlay";
+
+	let figureBox = document.createElement("figure");
+	overlay.appendChild(figureBox);
+	document.body.appendChild(overlay);
+
+    let overlayImage = document.createElement("img");
+    overlayImage.src = "img5.jpg";
+    figureBox.appendChild(overlayImage);
+
+    let closeBox = document.createElement("div");
+    closeBox.id = "artOverlayClose";
+    closeBox.innerHTML = "&times;";
+
+    closeBox.onclick = function() {
+        document.body.removeChild(overlay);
+    }
+
+    overlay.appendChild(closeBox);
+}
+		
