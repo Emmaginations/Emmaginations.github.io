53291a6b48d57cce5762b4251e0b95ec0c00f98c
Emmaginations <158538580+Emmaginations@users.noreply.github.com>
2025-04-07 23:32:34 -0400

    Update proj9.js

--- proj9.js
+++ proj9.js
@@ -2,4 +2,25 @@ window.addEventListener('DOMContentLoaded', () => {
     let qString = location.search.slice(1);
     console.log(qString);
 
+    qString = decodeURIComponent(qString);
+
+    let preferenceData = qString.split(/&/g);
+
+    for (let items of preferenceData) {
+        let fieldValuePair = items.split(/=/);
+        let fieldName = fieldValuePair[0];
+        let fieldValue = fieldValuePair[1];
+
+        if (fieldName === "bgcolor") {
+            document.body.style.backgroundColor = fieldValue;
+        } else if (fieldName === "txtcolor") {
+            document.body.style.color = fieldValue;
+        } else if (fieldName === "txtsize") {
+            document.body.style.fontSize = fieldValue;
+        }
+        
+    document.cookie = items;
+    }
+
+
 });
